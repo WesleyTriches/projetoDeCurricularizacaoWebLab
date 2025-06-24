921adbb1d3c52b2fb1f01cbd0afb5eb303700ee4
Rafgitpr <rluiznb@gmail.com>
2025-06-23 21:25:54 -0300

    sobreeBotoes

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ function menuShow() {
 }
 
 // codigo para fechar o menu ao clicar em um link
-
 const menuLinks = document.querySelectorAll('.mobile-menu a');
 menuLinks.forEach(link => { 
     link.addEventListener('click', () => {
@@ -37,3 +36,53 @@ if(buttonDesktop) {
 if(buttonMobile) {
   buttonMobile.addEventListener('click', toggleFontSize);
 }
+
+// botao de tema 
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+// mobile
+const themeToggleMobile = document.getElementById('theme-toggle-mobile');
+const themeIconMobile = document.getElementById('theme-icon-mobile');
+
+// atualiza o ícone conforme o tema atual
+function updateThemeIcon(theme) {
+    if (themeIcon) {
+        themeIcon.src = (theme === 'light') ? 'img/sol.svg' : 'img/lua.svg';
+    }
+    if (themeIconMobile) {
+        themeIconMobile.src = (theme === 'light') ? 'img/sol.svg' : 'img/lua.svg';
+    }
+    const mainImage = document.getElementById('main-image');
+    if (mainImage) {
+        mainImage.src = (theme === 'light') ? 'img/mainImg.png' : 'img/Dark theme.png';
+    }
+}
+
+// alternar tema
+function toggleTheme() {
+  const isLight = document.body.classList.toggle('light-mode');
+  const isDark = document.body.classList.toggle('dark-mode', !isLight);
+
+  const newTheme = isLight ? 'light' : 'dark';
+  updateThemeIcon(newTheme);
+  localStorage.setItem('theme', newTheme);
+}
+
+// clique
+if (themeToggle) {
+  themeToggle.addEventListener('click', toggleTheme);
+}
+
+// mobile
+if (themeToggleMobile) {
+  themeToggleMobile.addEventListener('click', toggleTheme);
+}
+
+// quando carregar a página, aplica tema salvo 
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'light') {
+  document.body.classList.add('light-mode');
+} else {
+  document.body.classList.add('dark-mode');
+}
+updateThemeIcon(savedTheme || 'dark');
